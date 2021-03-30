01a2d3f645718e639f58bfff684f50af23463e80
dionisiusst2 <dionisiusst2@gmail.com>
2021-03-30 14:52:55 +0700

    Implement redux for logs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {updateLog} from '../../actions/logActions';
 
-const EditLogModal = () => {
+const EditLogModal = ({current, updateLog}) => {
 	const [message, setMessage] = useState('');
 	const [attention, setAttention] = useState(false);
 	const [tech, setTech] = useState('');
@@ -10,12 +13,30 @@ const EditLogModal = () => {
 		if (message === '' || tech === '') {
 			M.toast({html: 'Please enter a message and tech'});
 		} else {
-			console.log(message, tech, attention);
+			const updatedLog = {
+				id: current.id,
+				message,
+				attention,
+				tech,
+				date: new Date(),
+			};
+
+			updateLog(updatedLog);
+			M.toast({html: `Log updated by ${tech}`});
+
 			setMessage('');
 			setTech('');
 			setAttention(false);
 		}
 	};
+
+	useEffect(() => {
+		if (current) {
+			setMessage(current.message);
+			setTech(current.tech);
+			setAttention(current.attention);
+		}
+	}, [current]);
 
 	return (
 		<div id='edit-log-modal' className='modal' style={modalStyle}>
@@ -29,9 +50,6 @@ const EditLogModal = () => {
 							value={message}
 							onChange={(e) => setMessage(e.target.value)}
 						/>
-						<label htmlFor='message' className='active'>
-							Log Message
-						</label>
 					</div>
 				</div>
 				<div className='row'>
@@ -86,4 +104,12 @@ const modalStyle = {
 	height: '75%',
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+	updateLog: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+	current: state.log.current,
+});
+
+export default connect(mapStateToProps, {updateLog})(EditLogModal);
